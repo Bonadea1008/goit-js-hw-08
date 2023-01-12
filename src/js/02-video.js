5fb97f3510f626fe267b99edcc366e40b7d2fb71
Anna <100880lar@gmail.com>
2023-01-12 17:18:05 +0200

    Add task 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,16 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
