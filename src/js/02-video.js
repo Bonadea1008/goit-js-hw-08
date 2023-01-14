0a5f1ae256e5f3fd4586bf17d2ad8587b4a80b40
Anna <100880lar@gmail.com>
2023-01-14 16:53:14 +0200

    Add JS for video playback

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,15 +3,17 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-
-player.on('timeupdate', throttle(onPlay, 1000));
-
-function onPlay({ seconds }) {
-  localStorage.setItem('videoplayer-current-time', seconds);
-}
-
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+const VIDEO_CURRENT_TIME = 'videoplayer-current-time';
 
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
 });
+
+const onPlay = function (data) {
+  const currentTime = data.seconds;
+  localStorage.setItem(VIDEO_CURRENT_TIME, currentTime);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem(VIDEO_CURRENT_TIME));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const formEmail = document.querySelector('.feedback-form input');
+const formMessage = document.querySelector('.feedback-form textarea');
+const STORAGE_KEY = 'feedback-form-state';
+const feedbackFormData = {};
+
+formEl.addEventListener('input', throttle(onFeedbackFormInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
+populateData();
+
+function onFeedbackFormInput(evt) {
+  feedbackFormData[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackFormData));
+}
+
+function populateData() {
+  const savedFeedbackData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedFeedbackData) {
+    formEmail.value = savedFeedbackData.email;
+    formMessage.value = savedFeedbackData.message;
+  }
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  evt.currentTarget.reset();
+
+  localStorage.removeItem(STORAGE_KEY);
+}
